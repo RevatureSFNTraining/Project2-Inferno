4083437095f718a1e221090430e36b27dbe468a4
bigcman120 <cliffordmjohnsonjr@gmail.com>
2022-01-31 13:00:02 -0500

    Aura and LWC Outlined
    Basic functionality for the components outlined.

--- Project 2 Inferno/force-app/main/default/aura/browsePageContainer/browsePageContainerController.js
+++ Project 2 Inferno/force-app/main/default/aura/browsePageContainer/browsePageContainerController.js
@@ -1,9 +1,12 @@
 ({
     handleSearch : function(component, event, helper) {
-        
+        helper.handleSearch(component, event.data);
     },
 
     handleDisplay : function (component, event, helper) {
-
+        helper.handleDisplay(component, event.data);
+    },
+    checkSearch : function (component, event, helper){
+        helper.checkSearch(component);
     },
 })
--- Project 2 Inferno/force-app/main/default/aura/browsePageContainer/browsePageContainerHelper.js
+++ Project 2 Inferno/force-app/main/default/aura/browsePageContainer/browsePageContainerHelper.js
@@ -1,9 +1,20 @@
 ({
-    searchHelper : function() {
-
+    handleSearch : function(component, String) {
+        component.set("v.searchString", String);
     },
 
-    displayHelper : function() {
-        
+    handleDisplay : function(cmp, query) {
+        if (query != null){
+            //Filter Movie Reels
+        }
+    },
+    
+    checkSearch : function(component){
+        if (component.get("v.searchString") == ""){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 })
