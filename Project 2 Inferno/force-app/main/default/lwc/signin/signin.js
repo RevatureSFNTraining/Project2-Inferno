edd248c8a6dacd59798e9a3410398f8229f53d73
Jeffrey Cooke <jcook051@fiu.edu>
2022-02-01 17:34:19 -0500

    Added LWC for sign in page

---
+++ Project 2 Inferno/force-app/main/default/lwc/signin/signin.js
@@ -0,0 +1,49 @@
+import { LightningElement, wire, api } from 'lwc';
+import getAccount from '@salesforce/apex/signinAccountController.getAccount';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+
+
+    const FIELDS = [
+        'Account.Name',
+        'Account.Email__c',
+        'Account.Membership_Status__c',
+        'Account.Password__c',
+        'Account.Name_On_Card__c',
+        'Account.Card_Number__c',
+        'Account.Card_Type__c',
+        'Account.CVV__c',
+        'Account.Expiration_Date__C',
+        'Account.Card_Status__c',
+        'Account.BillingAddress',
+    ];
+
+export default class Signin extends LightningElement {
+    email = '';
+    tempemail = '';
+    password = '';
+    myaccount = ACCOUNT_OBJECT;
+    id = 'test';
+    error;
+    //@wire(getAccount, {email: '$email'}) account;
+
+    async handleClick(event){
+        let myAccount = { 'sobjectType': 'Account' };
+        this.email = this.tempemail;
+        await getAccount({email: this.email}).then(result => {this.id = result;}).catch(error => {this.error = error;});
+        console.log(this.id);
+        if(!id==null){
+            this.dispatchEvent(new CustomEvent('signin', {detail: this.id}));
+        }
+
+    }
+
+    emailChange(event){
+        this.tempemail = event.detail.value;
+    }
+
+    passwordChange(event){
+        this.password = event.detail.value;
+    }
+}
