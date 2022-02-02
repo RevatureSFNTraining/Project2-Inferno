ce20e0c1b1176720cc02af90bab0697bb78419b0
Jeffrey Cooke <jcook051@fiu.edu>
2022-02-02 15:44:45 -0500

    Added 'search' LWC

--- Project 2 Inferno/force-app/main/default/lwc/search/search.js
+++ Project 2 Inferno/force-app/main/default/lwc/search/search.js
@@ -2,8 +2,16 @@ import { LightningElement } from 'lwc';
 
 export default class Search extends LightningElement {
     search;
+    searchresults;
 
     handleSearchChange(event){
         this.search = event.detail.value;
+    }
+
+    async handleSearch(event){
+        await searchResults({text: this.search}).then(result => {this.searchresults = result;}).catch(error => {this.error = error;});
+        if(!this.searchresults==null){
+            this.dispatchEvent(new CustomEvent('search', {detail: this.searchresults}));
+        }
     }
 }
